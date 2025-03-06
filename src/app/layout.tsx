fed2ed8f867ce8bf0a8a46774da29a8464d520c5
Joel Vitor <joel.vitor.bravo@gmail.com>
2025-03-05 21:11:39 -0300

    feat: preload de fontes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,16 @@ import "./globals.css";
 import { DM_Sans } from "next/font/google"
 
 const dmSans = DM_Sans({
+  subsets: ["latin"],
   weight: ["400", "700"],
+  preload: true,
   variable: "--font-dm-sans"
 })
 
+export const metadata = {
+  title: 'Front Desafio - Russel',
+  description: 'Desafio de front promovido pela empresa Russel Serviços.'
+}
 
 export default function RootLayout({
   children,
@@ -14,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt">
       <body className={dmSans.className}
       >
         {children}
